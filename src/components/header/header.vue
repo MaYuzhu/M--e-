95d373a2e9f87de72bae09964aeb34109fb14d8d
MaYuzhu <mayuzhu123@sina.com>
2018-04-19 17:29:15 +0800

    'day01'

---
+++ src/components/header/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header_wrap">
+    <div class="logo">
+      <img src="./logo.png" alt="">
+    </div>
+    <div class="nav">
+      <i class="iconfont icon-gengduo"></i>
+      <i class="iconfont icon-ego-guanbi"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .header_wrap
+    width 100%
+    height 68px
+    background pink
+    .logo
+      width 160px
+      height 100%
+      display flex
+      justify-content center
+      align-items center
+      img
+        width 100%
+</style>
