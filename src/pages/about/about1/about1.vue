fdcd2f72e9a86aebe9f17a33d5280323c0d8e2d5
MaYuzhu <mayuzhu123@sina.com>
2018-05-04 13:33:47 +0800

    5.4周5  关于我们页完成

--- src/pages/about/about1/about1.vue
+++ src/pages/about/about1/about1.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
+  <div class="about_wrap">
     <Title title="公司简介"></Title>
-    <div style="height:1000px; background: pink"></div>
+    <p>中企智融金融服务外包（北京）有限公司成立于2016年，股东为汉唐控股集团和黑龙江国投政信资产交易中心有限公司，注册资本金1亿元。</p>
+    <br/>
+    <p>中企智融金融服务外包（北京）有限公司总部位于北京朝阳区CBD核心区域。平台业务网点辐射全国，分布北京、天津、哈尔滨、齐齐哈尔、邯郸等地，形成了既有集中又有分散，既有统一性又有灵活性，适应复杂多变市场条件的高效率运行的公司管理运营体系，成为一家以互联网金融、商业保理、私募基金、融资租赁等业务为一身的多元化、跨区域的大型金融企业，为大众提供全方位的财富管理服务。</p>
+    <img class="map" src="./aboutDitu.gif" alt="">
+    <p>中企智融致力于为高成长性人群、大众富裕阶层和高净值客户提供全方位、多层次、个性化进行全方位的资产配置。通过深研宏观经济、投资行业等发展态势，深入了解客户财务状况，为客户提供专业性建议，为助力客户实现家庭财富保驾护航。集团以雄厚的资金实力、高素质的专业人才和优质高效的服务在社会上树立起了良好的企业形象、，受到了社会各界与广大客户的依赖与赞扬。</p>
+    <div class="img_wrap">
+      <img src="./left02.gif" alt="">
+      <img src="./left03.gif" alt="">
+    </div>
+    <div class="linian">
+      <h2>中企智融理念</h2>
+      <div></div>
+    </div>
+    <div class="about_footer">
+      <div>
+        <img src="./xin.gif" alt="">
+        <h2>企业理念</h2>
+        <p>让每一位客户都能享受到普惠金融的魅力</p>
+      </div>
+      <div>
+        <img src="./lingdai.gif" alt="">
+        <h2>企业价值观</h2>
+        <p>信用、专业、创新、品质</p>
+      </div>
+      <div>
+        <img src="./wang.gif" alt="">
+        <h2>企业使命</h2>
+        <p>为中国金融机构及类金融机构的资产证券化提供强大的流动性平台，建立庞大的金融资产交易体系为经营理念的互联网金融服务机构</p>
+      </div>
+      <div>
+        <img src="./diannao.gif" alt="">
+        <h2>企业愿景</h2>
+        <p>依托国有企业、中央企业的公信力，成为多元化经营、多方位发展、多样化产品特点的综合性金融服务平台。</p>
+      </div>
+    </div>
+    <div class="about_footer2">
+      <h2>我们的承诺</h2>
+      <div></div>
+      <p>只提供第三方金融服务，不经手客户资金；
+        严格履行保密协议，不透漏客户信息</p>
+
+    </div>
   </div>
 </template>
 
@@ -15,5 +56,90 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .about_wrap
+    >p
+      width 100%
+      margin 0px auto
+      padding 0 20px
+      box-sizing border-box
+      text-indent:2em
+      text-align justify
+      letter-spacing 1px
+      font-size 14px
+      line-height 2.4
+    .map
+      width 100%
+    .img_wrap
+      width 100%
+      height 113px
+      overflow hidden
+      display flex
+      justify-content center
+      img
+        width 50%
+        height 100%
+    .linian
+      height 50px
+      background #f9662d
+      h2
+        font-size 20px
+        color #fff
+        text-align center
+        line-height 50px
+      div
+        width 46px
+        height 1px
+        background #fff
+        margin -8px auto 0
+    >.about_footer
+      width 100%
+      margin 0 auto
+      display flex
+      justify-content center
+      flex-wrap wrap
+      padding 20px 0
+      >div
+        width 50%
+        img
+          display block
+          margin 16px auto
+        h2
+          width 100%
+          text-align center
+          font-size 14px
+          letter-spacing 1px
+        p
+          width 100%
+          padding 4px 8px
+          box-sizing border-box
+          text-align center
+          font-size 14px
+          line-height 1.8
+          letter-spacing 1px
+          color #999
+    >.about_footer2
+      width 100%
+      margin 0 auto
+      background rgb(248,248,248)
+      padding 14px 0
+      h2
+        font-size 18px
+        width 100%
+        text-align center
+        letter-spacing 3px
+        font-weight 600
+      >div
+        width 60px
+        height 1px
+        background #a0a0a0
+        margin 10px auto 0
+      >p
+        font-size 16px
+        margin 4px auto
+        padding 0 10px
+        line-height 40px
+        color #666
+        letter-spacing 1px
+        text-align center
+        vertical-align top
 </style>
--- src/pages/about/about2/about2.vue
+++ src/pages/about/about2/about2.vue
@@ -1,13 +1,270 @@
 <template>
   <div>
-    集团优势
+    <Title title="集团优势"></Title>
+    <div class="content1">
+      <p>持续稳健的发展，使得中企智融能够不间断的发展独特优势，并在行业内持续的保有品牌的权威性、强大的综合竞争能力和旺盛的创新研发能力。</p>
+      <div class="num1_wrap">
+        <img src="./num1.png" alt="">
+        <p class="content1_text">行业内服务累积的品牌权威性</p>
+      </div>
+
+      <ul>
+        <li>
+          <img src="./c1-1.png" alt="">
+          <p>令人称道的服务质量和客户满意度</p>
+          <p>企业提高优质高效的服务，深入了解每一位客户自身情况，并为其提供专业性建议，为助力客户实现财富保值增值。</p>
+        </li>
+        <li>
+          <img src="./c1-2.png" alt="" style="margin:36px auto 68px">
+          <p>中国中小企业协会会员单位</p>
+          <p>中企智融致力于为大众提供全方位的财富管理服务，促进互联网金融行业的发展，以实现中国企业的变革和振兴为己任；</p>
+        </li>
+      </ul>
+    </div>
+    <div class="content2">
+      <div class="inner2">
+        <div class="num2_wrap">
+          <img src="./num2.png" alt="">
+          <p class="content2_text">丰富的金融行业经验，强大的综合竞争能力</p>
+        </div>
+
+        <ul>
+          <li>
+            <img src="./c2-1.png" alt="">
+            <p>企业团队规模大、人员结构完整</p>
+            <p>由金融管理专家、人力资源专家和行业资深高管共同组成并不断吸引人才</p>
+          </li>
+          <li>
+            <img src="./c2-2.png" alt="">
+            <p>行业经验丰富、覆盖范围广泛</p>
+            <p>从事金融行业多年；涉足票据、众筹、地产、资产管理等行业，行业经验丰富。</p>
+          </li>
+          <li>
+            <img src="./c2-3.png" alt="">
+            <p>共享管理经验，提升综合竞争能力</p>
+            <p>利用集团的共有平台，将优秀管理制度、丰富经验进行复制输出，利用制度资本进行扩张；另一方面将最佳管理实践进行传播，在集团范围内进行管理和创新的提升。</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="content3">
+      <div class="inner3">
+        <div class="num3_wrap">
+          <img src="./num3.jpg" alt="">
+          <p class="content3_text">专业、热情并极富创造力的团队，旺盛的研发能力</p>
+        </div>
+        <ul>
+          <li class="li1">
+            <img class="img1" src="./c3-1.png" alt="">
+            <p class="p1">特设探索前沿理论的专家团队</p>
+            <p class="p2">联合行业专业人才，组建专家团队，研究行业问题和前沿理论，通过庞大行业人力资本数据库，积累大量行业问题和解决方案，形成独有的知识库；</p>
+          </li>
+          <li class="li2">
+            <div class="p_wrap">
+              <p class="p3">大量人才储备和人才合作，确保企业服务的旺盛生命力</p>
+              <p class="p4">中企智融与国内多所高校进行合作，为后续专业人才的培养和发展不断的提供新鲜血液；中企智融坚持合作共赢的模式，和大量企业进行合作，维护行业高端人才库，确保金融行业粘性和落地性，提供有力的行业支持。</p>
+            </div>
+
+            <img class="img2" src="./c3-2.jpg" alt="">
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import Title from '../../../components/titles/title.vue'
+  export default {
+    components:{
+      Title,
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .content1
+    width 100%
+    padding-bottom 40px
+    >p
+      width 90%
+      margin 20px auto 0
+      font-size 16px
+      letter-spacing 2px
+      line-height 1.8
+      color #787878
+      text-align justify
+    .num1_wrap
+      width 100%
+      margin 0 auto
+      position relative
+      >img
+        display block
+        margin 50px auto
+        transform scale(0.8)
+      .content1_text
+        position absolute
+        top 50%
+        left 50%
+        transform translate(-50%,-50%)
+        width 200px
+        font-size 14px
+        color #606060
+        background rgba(255,255,255,.7)
+        line-height 24px
+        letter-spacing 1px
+
+    >ul
+      width 100%
+      margin 0 auto
+      display flex
+      >li
+        width 50%
+        border-right 1px solid #ddd
+        img
+          display block
+          margin 0 auto 50px
+          width 140px
+        >:nth-child(2)
+          width 80%
+          margin 0 auto
+          letter-spacing 2px
+          text-align center
+          line-height 1.4
+          color #3b3b3b
+          font-size 14px
+        >:nth-child(3)
+          width 90%
+          margin 10px auto 0
+          text-align justify
+          color #787878
+          font-size 12px
+          line-height 2
+      >:nth-child(2)
+        border-right none
+        >:nth-child(3)
+          width 90%
+
+  .content2
+    width 100%
+    background #f3f3f3
+    .inner2
+      width 100%
+      margin 0 auto
+      .num2_wrap
+        width 100%
+        position relative
+        padding 50px 0
+        >img
+          display block
+          margin 0 auto
+        .content2_text
+          position absolute
+          top 50%
+          left 50%
+          text-align center
+          transform translate(-50%,-50%)
+          font-size 20px
+          color #606060
+          background rgba(243,243,243,.7)
+          line-height 28px
+          letter-spacing 1px
+
+      >ul
+        width 100%
+        display flex
+        justify-content space-between
+        >li
+          p
+            width 320px
+            margin 0 auto
+          >:nth-child(2)
+            font-size 16px
+            color #3b3b3b
+            margin-top 50px
+            text-align center
+          >:nth-child(3)
+            font-size 16px
+            color #787878
+            margin 30px 0 50px
+            text-align justify
+            line-height 2
+            letter-spacing 1px
+
+
+  .content3
+    width 100%
+    margin-bottom 80px
+    .inner3
+      width 100%
+      margin 0 auto
+      .num3_wrap
+        width 100%
+        position relative
+        padding 50px 0
+        >img
+          display block
+          margin 0 auto
+        .content3_text
+          position absolute
+          top 50%
+          left 50%
+          text-align center
+          transform translate(-50%,-50%)
+          font-size 20px
+          color #606060
+          background rgba(255,255,255,.7)
+          line-height 28px
+          letter-spacing 1px
+
+      >ul
+        width 100%
+        >.li1
+          overflow hidden
+          margin-bottom 30px
+          >.img1
+            float left
+          >.img2
+            float right
+          >.p1
+            font-size 16px
+            color #3b3b3b
+            margin 60px 0 0 50px
+            float left
+            line-height 2
+            letter-spacing 1px
+          >.p2
+            font-size 16px
+            color #787878
+            margin 30px 0 50px 50px
+            text-align justify
+            line-height 2.5
+            letter-spacing 1px
+            float left
+            width 650px
+
+
+        >.li2
+          width 100%
+          overflow hidden
+          .p_wrap
+            float left
+            width 650px
+          >.img2
+            float right
+          .p3
+            font-size 16px
+            color #3b3b3b
+            margin-top 40px
+            line-height 2
+            letter-spacing 1px
+          .p4
+            font-size 16px
+            color #787878
+            margin 30px 0 50px
+            text-align justify
+            line-height 2.5
+            letter-spacing 1px
+
 
 </style>
